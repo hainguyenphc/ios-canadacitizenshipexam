e086312088b00e82979be2bf7ae159234ed4b6ff
Hai Nguyen <hnguyen@northern.co>
2023-01-28 17:16:42 -0500

    the Home view model

---
+++ canadacitizenshipexam/ScreenLevelVC/Home/Logic/HomeVM_.swift
@@ -0,0 +1,47 @@
+//
+//  HomeVM_.swift
+//  canadacitizenshipexam
+//
+//  Created by hainguyen on 2023-01-28.
+//
+
+import Foundation
+import FirebaseAuth
+
+protocol HomeVMDelegate {
+
+  func loadPracticeProgress()
+
+}
+
+class HomeVM_: HomeVMDelegate {
+
+  var delegate: HomeVCDelegate?
+
+  func loadPracticeProgress() {
+    guard let userID = Auth.auth().currentUser?.uid else {
+      return
+    }
+
+    NetworkManager.shared.getUsersData(userID: userID) { result in
+      switch (result) {
+        case .success(let usersData):
+          self.delegate?.handleSuccessLoadingUsersDataFromNetworkCall(usersData: usersData)
+        case .failure(let error):
+          self.delegate?.handleErrorLoadingUsersDataFromNetworkCall(error: error)
+      }
+    }
+  }
+
+  func checkAuthorization() {
+    guard let _ = Auth.auth().currentUser else {
+      if let delegate = delegate {
+        (delegate as! HomeVC_)
+          .navigationController?
+          .pushViewController(RegisterVC(), animated: true)
+      }
+      return
+    }
+  }
+
+}
