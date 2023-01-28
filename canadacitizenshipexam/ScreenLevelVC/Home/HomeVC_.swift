82a6dd3d658e4fa3b2a96d634ad589c1796640c4
Hai Nguyen <hnguyen@northern.co>
2023-01-27 21:06:37 -0500

    this is gonna replace the old HomeVC eventually

--- canadacitizenshipexam/ScreenLevelVC/Home/HomeVC_.swift
+++ canadacitizenshipexam/ScreenLevelVC/Home/HomeVC_.swift
@@ -5,4 +5,19 @@
 //  Created by hainguyen on 2023-01-27.
 //
 
-import Foundation
+import UIKit
+
+class HomeVC_: UIViewController {
+
+  let bounds = UIScreen.main.bounds
+  let deviceIdiom = UIScreen.main.traitCollection.userInterfaceIdiom
+
+  // @remove headerBackgroundView
+  var stickyHeadingView: UIView!
+
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+    self.setupStickyHeadingView()
+  }
+
+}
