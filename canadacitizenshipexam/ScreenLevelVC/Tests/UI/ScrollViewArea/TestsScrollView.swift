63a98b081ed56b49a74a2fb0cf15a9ed5264f0ab
Hai Nguyen <hnguyen@northern.co>
2023-01-31 07:35:23 -0500

    the UI for TestsVC

---
+++ canadacitizenshipexam/ScreenLevelVC/Tests/UI/ScrollViewArea/TestsScrollView.swift
@@ -0,0 +1,49 @@
+//
+//  TestsView.swift
+//  canadacitizenshipexam
+//
+//  Created by hainguyen on 2023-01-31.
+//
+
+import UIKit
+
+/*
+ Build the UI of TestsVC_.
+ */
+extension TestsVC_: ScrollProtocol {
+
+  func setupScrollView() {
+    scrollView.overrideUserInterfaceStyle = .light
+    scrollView.translatesAutoresizingMaskIntoConstraints = false
+    scrollView.isScrollEnabled = true
+    view.addSubview(scrollView)
+
+    NSLayoutConstraint.activate([
+      scrollView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: SCROLL_VIEW_LEFT_AND_RIGHT_SPACE),
+      scrollView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -SCROLL_VIEW_LEFT_AND_RIGHT_SPACE),
+      scrollView.heightAnchor.constraint(equalToConstant: BOUNDS.height * 0.7)
+    ])
+
+    var cards: [CardProtocol] = []
+
+    var x: CardProtocol? = nil
+    var previousView: UIView? = nil
+    var distanceToTop: CGFloat = 0
+    var index = 0
+    for each in cards {
+      x = each.build(scrollView: scrollView, previous: (index == 0) ? nil : previousView)
+      NSLayoutConstraint.activate([
+        (x!).theView!.topAnchor.constraint(equalTo: scrollView.topAnchor, constant: distanceToTop),
+        (x!).theView!.centerXAnchor.constraint(equalTo: scrollView.centerXAnchor),
+        (x!).theView!.heightAnchor.constraint(equalToConstant: each.theHeight!),
+        (x!).theView!.widthAnchor.constraint(equalToConstant: BOUNDS.width * 0.95)
+      ])
+      previousView = (each).theView
+      distanceToTop += each.theHeight! + DISTANCE_BETWEEN_CARDS
+      index += 1
+    }
+
+    scrollView.contentSize = CGSize(width: 100, height: distanceToTop * 2)
+  }
+
+}
