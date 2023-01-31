3fd8a4f8471e6c7685ec3605b03921d540028356
Hai Nguyen <hnguyen@northern.co>
2023-01-31 07:33:51 -0500

    the boilerplate for the TestsVC_

---
+++ canadacitizenshipexam/ScreenLevelVC/Tests/TestsVC_.swift
@@ -0,0 +1,34 @@
+//
+//  TestsVC_.swift
+//  canadacitizenshipexam
+//
+//  Created by hainguyen on 2023-01-31.
+//
+
+import UIKit
+
+protocol TestsVCDelegate {
+  
+}
+
+class TestsVC_: UIViewController, TestsVCDelegate {
+
+  var unlockPremiumFeaturesView: UIView?
+
+  // The scroll view containing several cards.
+  var scrollView: UIScrollView! = UIScrollView()
+
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+
+    // Build the Unlock view first since other views depend on it.
+    unlockPremiumFeaturesView = buildTheUnlockPremiumFeatureView()
+  }
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+
+
+
+}
