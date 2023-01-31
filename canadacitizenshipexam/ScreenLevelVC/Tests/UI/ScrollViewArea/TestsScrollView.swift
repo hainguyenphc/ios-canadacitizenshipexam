1b02100bdd95f5cb3d263bc7da72f2fd3d8401f9
Hai Nguyen <hnguyen@northern.co>
2023-01-31 08:40:45 -0500

    render the heading temporarily

--- canadacitizenshipexam/ScreenLevelVC/Tests/UI/ScrollViewArea/TestsScrollView.swift
+++ canadacitizenshipexam/ScreenLevelVC/Tests/UI/ScrollViewArea/TestsScrollView.swift
@@ -34,6 +34,12 @@ extension TestsVC_: ScrollProtocol {
       scrollView.heightAnchor.constraint(equalToConstant: BOUNDS.height * 0.7)
     ])
 
+    //
+
+    f()
+
+    //
+
     var cards: [CardProtocol] = []
 
     var x: CardProtocol? = nil
@@ -54,6 +60,77 @@ extension TestsVC_: ScrollProtocol {
     }
 
     scrollView.contentSize = CGSize(width: 100, height: distanceToTop * 2)
+  }
+
+  func f() {
+    let titleLabel = UIBuildingManager.shared.buildTheTitleLabel(title: "Practice Tests", textColor: .label)
+    let taglineLabel = UIBuildingManager.shared.buildTheTaglineLabel(tagline: "35 Tests", textColor: .secondaryLabel)
+    let imageView = UIBuildingManager.shared.buildTheImageNextToTitleLabel(systemName: "")
+    let options = CompletionPercentageOptions(position: .left, textColor: UIColor.label.cgColor)
+    let completionPercentageView = UIBuildingManager.shared.buildTheCompletionPercentageView(percent: 0.5, options: options)
+    let completionDetailsView = UIBuildingManager.shared.buildTheCompletionDetailsView(textColor: .label)
+
+    self.completionPercentageView = completionPercentageView
+
+    self.completionCircularProgressView = completionPercentageView?
+      .subviews
+      .compactMap{$0 as? CircularProgressView}
+      .first
+
+    (self.completionCircularProgressView)!.progressColor = .gray
+
+    self.completionPercentageLabel = completionPercentageView?
+      .subviews
+      .compactMap{$0 as? UILabel}
+      .first
+
+    let hStack = UIStackView()
+    hStack.axis = .horizontal
+    hStack.alignment = .center
+    hStack.distribution = .fill
+    hStack.spacing = 10
+    hStack.translatesAutoresizingMaskIntoConstraints = false
+
+    view.addSubview(hStack)
+
+    NSLayoutConstraint.activate([
+      hStack.topAnchor.constraint(equalTo: unlockPremiumFeaturesView != nil ? unlockPremiumFeaturesView!.bottomAnchor : view.safeAreaLayoutGuide.topAnchor, constant: 10),
+      hStack.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: LEFT_SPACE),
+      hStack.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -10),
+      hStack.heightAnchor.constraint(equalToConstant: BOUNDS.height * 0.05)
+    ])
+
+    hStack.addArrangedSubview(titleLabel)
+
+    view.addSubview(hStack)
+
+    if let taglineLabel = taglineLabel {
+      view.addSubview(taglineLabel)
+      NSLayoutConstraint.activate([
+        taglineLabel.topAnchor.constraint(equalTo: hStack.bottomAnchor, constant: 5),
+        taglineLabel.leadingAnchor.constraint(equalTo: hStack.leadingAnchor)
+      ])
+    }
+
+    if let blockView = completionPercentageView {
+      view.addSubview(blockView)
+      NSLayoutConstraint.activate([
+        blockView.topAnchor.constraint(equalTo: taglineLabel != nil ? taglineLabel!.bottomAnchor : titleLabel.bottomAnchor, constant: 20),
+        blockView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: LEFT_SPACE),
+        blockView.heightAnchor.constraint(equalToConstant: 60),
+        blockView.widthAnchor.constraint(equalToConstant: 60)
+      ])
+    }
+
+    if let completionDetailsView = completionDetailsView, let previousBlock = completionPercentageView {
+      view.addSubview(completionDetailsView)
+      NSLayoutConstraint.activate([
+        completionDetailsView.topAnchor.constraint(equalTo: previousBlock.topAnchor),
+        completionDetailsView.leadingAnchor.constraint(equalTo: previousBlock.trailingAnchor, constant: 20),
+        completionDetailsView.heightAnchor.constraint(equalToConstant: 60),
+        completionDetailsView.widthAnchor.constraint(equalToConstant: 300)
+      ])
+    }
   }
 
 }
